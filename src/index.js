6606bc3c335bb26b9a4c351380247d7c3511ae88
Zia123456 <m.ziabakhteyari12@gmail.com>
2022-11-03 23:35:56 -0700

    sort function added

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ const listDiv = document.querySelector('.to-do-list-container');
 
 // array of to do object
 const list = [
-  { description: 'was the dishes', completed: false, index: 1 },
-  { description: 'was the dishes', completed: false, index: 2 },
-  { description: 'was the dishes', completed: false, index: 3 },
+  { description: 'was the dishes-4', completed: false, index: 4 },
+  { description: 'was the dishes-1', completed: false, index: 1 },
+  { description: 'was the dishes-3', completed: false, index: 3 },
+  { description: 'was the dishes-2', completed: false, index: 2 },
 ];
 
 // function to display to do list
 const displayList = function display() {
+  // js to sort array of objects by index value
+  list.sort((a, b) => a.index - b.index);
+  // js to iterate through array of object and display sorted array
   for (let i = 0; i < list.length; i += 1) {
     const toDoDiv = document.createElement('div');
     toDoDiv.className = 'to-do';
